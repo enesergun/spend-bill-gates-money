52c544c78a0df57c363a81848295cc7e497ae2c9
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2022-02-01 12:55:34 +0300

    App.js wallet tamamlandi

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ function App() {
         <img src={bill_gates} alt="Bill Gates"/>
         <h1>Spend Bill Gates's Money</h1>
       </div>
-      <div className="wallet"></div>
+      <div className="wallet">
+        $100,000,000,000
+      </div>
       <div className="products"></div>
     </div>
   );
