4476c44458ec7e9dafca84bb9bbcba2e7deee347
enesergun1515@gmail.com <63712936+enesergun@users.noreply.github.com>
2022-02-01 12:50:18 +0300

    header tamamlandi App.js icinde

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import bill_gates from './assets/images/bill_gates.png';
+;
+
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="header">
+        <img src={bill_gates} alt="Bill Gates"/>
+        <h1>Spend Bill Gates's Money</h1>
+      </div>
+      <div className="wallet"></div>
+      <div className="products"></div>
     </div>
   );
 }
